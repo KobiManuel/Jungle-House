66efbb7f17e8e0a4510cea1f9f4260dd4d1c913b
KobiManuel <Emmakobi91@gmail.com>
2022-11-18 17:59:09 +0100

    feat: added an alert to display based on the amount of sunlight or water each plant needs

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,10 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
-
+const quantityLabel = {
+	1: 'little',
+	2: 'moderate',
+	3: 'much'
+}
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
@@ -11,7 +15,13 @@ function CareScale({ scaleValue, careType }) {
 		)
 
 	return (
-		<div>
+		<div onClick={() =>
+            alert(
+                `This plant requires ${quantityLabel[scaleValue]} ${
+                    careType === 'light' ? 'light' : "watering"
+                }`
+            )
+        }>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
