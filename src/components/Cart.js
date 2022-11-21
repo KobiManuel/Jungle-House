94e355e96ecc5a0a0111a8388676242b8322832c
KobiManuel <Emmakobi91@gmail.com>
2022-11-21 12:02:37 +0100

    cleared some features

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,55 +1,22 @@
 import '../styles/Cart.css'
 import { useState } from 'react'
-import { useEffect } from 'react'
-function Cart({ cart, updateCart }) {
-	const [isOpen, setIsOpen] = useState(true)
+function Cart() {
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
 
-	const items = Object.keys(cart)
-	const total = items.reduce(
-		(acc, item) => acc + cart[item].amount * cart[item].price,
-		0
-	)
-	useEffect(() => {
-		alert(`You have ${total}€ to pay 💸`)
-	})
-
-	return isOpen ? (
-		<div className='jh-cart'>
-			<button
-				className='jh-cart-toggle-button'
-				onClick={() => setIsOpen(false)}
-			>
-				Close
-			</button>
-			{items.length > 0 ? (
-				<div>
-					<h2>Panier</h2>
-					<ul>
-						{items.map((item) => (
-							<li key={item}>
-								{cart[item]?.amount} {item} {cart[item]?.price}€
-							</li>
-						))}
-					</ul>
-					<h3>Total :{total}€</h3>
-					<button onClick={() => updateCart({})}>Empty the cart</button>
-				</div>
-			) : (
-				<div>Your cart is empty</div>
-			)}
-		</div>
-	) : (
-		<div className='jh-cart-closed'>
-			<button
-				className='jh-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
-				Open Cart
-			</button>
-		</div>
-	)
+    return (
+        <div className='jh-cart'>
+            <h2>Cart</h2>
+            <div>
+                Monstera: {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Add
+                </button>
+            </div>
+            <h3>Total: {monsteraPrice * cart}€</h3>
+        </div>
+    )
 }
-
 export default Cart
 /*function Cart() {
     const monsteraPrice = 8
